7fde9d8837c260c9727202c1c7fcf71f91b4f170
Axe10rellana <axelorellana550@gmail.com>
2023-11-15 13:43:05 -0300

    snapgram

--- vite.config.common.ts
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-export const commonConfig = {
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import VitePages from "vite-plugin-react-pages";
-import { commonConfig } from "./vite.config.common";
 
 export default defineConfig({
-  ...commonConfig,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   optimizeDeps: {
     include: ["react-router-dom"],
